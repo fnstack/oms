cdb3706f77adb68c693ea43406bdda6b1aa4b0e7
Claudin Babela <claudinbabs@gmail.com>
2019-04-20 16:25:06 +0100

    Did pagination to the repository.

--- src/OMS.DataAccess/ProductCategories/ProductCategory.Repository.fs
+++ src/OMS.DataAccess/ProductCategories/ProductCategory.Repository.fs
@@ -1,14 +1,26 @@
 [<AutoOpen>]
 module OMS.Data.ProductCategoryRepository
 
+open System
 open MongoDB.Bson
 open MongoDB.Driver
 open MongoDB.Driver
 open OMS.Application
 
 let getProductCategories (collection : IMongoCollection<ProductCategoryDto>) =
+    let totalDocuments = collection.CountDocuments(Builders.Filter.Empty) |> Convert.ToDecimal
+    
+    let pageSize = 2m
+    let currentPage = 2
+    let totalPages = Math.Ceiling(totalDocuments/pageSize) 
+    
+    let skip = Nullable<int>.op_Implicit ((currentPage - 1) * (pageSize |> Convert.ToInt32))
+    
     let categories =
-        collection.Find(Builders.Filter.Empty).ToEnumerable() |> Seq.toArray
+        collection.Find(Builders.Filter.Empty)
+            .Skip(skip)
+            .Limit(Nullable<int>.op_Implicit (pageSize |> Convert.ToInt32))
+            .ToEnumerable() |> Seq.toArray
     categories
 
 let createProductCategory (collection : IMongoCollection<ProductCategoryDto>,
